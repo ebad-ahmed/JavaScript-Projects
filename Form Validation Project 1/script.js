39fd8187fe114db97854b513526ddb3abbdf78b7
ebad-ahmed <52370487+ebad-ahmed@users.noreply.github.com>
2021-02-15 21:40:20 +0500

    creating Js File

---
+++ Form Validation Project 1/script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email')
+const password = document.getElementById('[password')
+const password2 = document.getElementById('password2')
+
+//All Functions
+
+//function to show error
+function showError (input,message) {
+    const formControl = input.parentElement; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//event on sumbit button
+form.addEventListener('submit',function(e) {
+e.preventDefault();
+
+
+
+
+})
