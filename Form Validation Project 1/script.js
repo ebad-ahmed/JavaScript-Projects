b05f9ffe17725b244cceff199c0c970f5497d0cf
ebad-ahmed <52370487+ebad-ahmed@users.noreply.github.com>
2021-02-16 17:03:59 +0500

    completed js file with correction

--- Form Validation Project 1/script.js
+++ Form Validation Project 1/script.js
@@ -5,53 +5,60 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+//creating functions
+
+//function to show error
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+//function to show succes
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//function to show email valid or not
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  //regular expression se check hogi pori email
+    return re.test(String(email).toLowerCase());    //phr return hojaegi email
+}
+
+
+
+
+
 //eventlistener
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-
+    //console.log(password.value);
     if (username.value === '') {
-        showError(username, 'Username is required');
+        showError(username, 'Username is required')
     } else {
         showSuccess(username);
     }
 
     if (email.value === '') {
-        showError(email, 'Email is Required')
+        showError(email, 'Email is required');
     } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is Invalid')
-    } else {
+        showError(email, 'Email is not invalid')
+    }
+    else {
         showSuccess(email);
     }
 
     if (password.value === '') {
-        showError(password, 'Password is Required')
+        showError(password, 'Password is required')
     } else {
         showSuccess(password);
     }
 
     if (password2.value === '') {
-        showError(password2, 'Confirm Password is required')
+        showError(password2, 'Confirm password is required')
     } else {
         showSuccess(password2);
     }
-
 })
 
-function showError(input, message) {
-    const formControl = input.parentElement;               //input ke parent ko access krrhe hain take class name change krlein override krke
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');      //querySelector se access krskte hain hm id,class,element
-    small.innerText = message;
-
-}
-
-function showSuccess(input){
-    const formControl = input.parentElement;               //input ke parent ko access krrhe hain take class name change krlein override krke
-    formControl.className= 'form-control success';
-}
-
-
-function isValidEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  //regular expression se check hogi pori email
-    return re.test(String(email).toLowerCase());    //phr return hojaegi email
-}
