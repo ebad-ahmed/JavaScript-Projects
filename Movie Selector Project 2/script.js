431fe2b1ee2664d1947c9302a1f8d0ecb573a039
ebad-ahmed <52370487+ebad-ahmed@users.noreply.github.com>
2021-02-20 03:24:36 +0500

    js completed

--- Movie Selector Project 2/script.js
+++ Movie Selector Project 2/script.js
@@ -4,21 +4,30 @@ const seats = document.querySelectorAll('.row.seat:not(.occupied)');
 const total = document.getElementById('total');
 const count = document.getElementById('count');
 const movieSelect = document.getElementById('movie');
-const ticketPrice = +movieSelect.Value;
+let ticketPrice = +movieSelect.value;
 
-////////console.log(ticketPrice);
+//console.log(ticketPrice);
 
-const updateSelectedCount = () => {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    //console.log(selectedSeats);
-    const countSelectedSeats = selectedSeats.length;
-    //console.log(countSelectedSeats);
 
-count.innerText = countSelectedSeats;
-total.innerText = ticketPrice * countSelectedSeats;
+populateUI();
 
+//pull data from local storage to bulid UI
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+if (selectedSeats !== null && selectedSeats.length > 0 ){
+    seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) > -1 ) {
+            seat.classList.add('selected');
+        }
+    });
 }
 
+const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+}
+}
 
 
 //event listner for click on available seat
@@ -32,3 +41,38 @@ container.addEventListener('click', (e) => {
     updateSelectedCount();
     
 })
+
+//Function to update counts
+
+const updateSelectedCount = () => {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    //console.log(selectedSeats)
+    const countSelectedSeats = selectedSeats.length;
+    //console.log(countSelectedSeats);
+
+    const seatsIndex = [...selectedSeats].map(seat =>[...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
+
+count.innerText = countSelectedSeats;
+total.innerText = ticketPrice * countSelectedSeats;
+}
+
+// Fucntion to save selected movie & its price
+function setMovieData(movieIndex,moviePrice){
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedMoviePrice',moviePrice);
+
+}
+
+
+// event listner for change of select movie in dropdown
+
+movieSelect.addEventListener('change', (e) => {
+
+    ticketPrice = +e.target.value;
+   setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+})
+
+// calculate initial number of seats and total price
+ updateSelectedCount();
