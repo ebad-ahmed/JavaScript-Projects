463f41cd27acf3b8db043046a9081d025c5b96cd
ebad-ahmed <52370487+ebad-ahmed@users.noreply.github.com>
2021-03-07 03:22:19 +0500

    Js file created

---
+++ DOM Array Methods Project 5/script.js
@@ -0,0 +1,15 @@
+const main = document.getElementById('main');
+const addUserButton = document.getElementById('add-user');
+const doubleMoneyButton = document.getElementById('double-money');
+const showMillionaireButton = document.getElementById('show-millionaire');
+const sortButton = document.getElementById('sort-richest');
+const totalButton = document.getElementById('calculate-total');
+
+// Initializing Data Array 
+let data = [];
+
+//functin to fetch random user
+// used https://randomuser.me/api/
+
+
+
