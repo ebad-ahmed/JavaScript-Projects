ac20bc868017fadd5998b42a4b87b036b3e298ff
ebad-ahmed <52370487+ebad-ahmed@users.noreply.github.com>
2021-03-15 21:55:28 +0500

    working js file

--- DOM Array Methods Project 5/script.js
+++ DOM Array Methods Project 5/script.js
@@ -1,62 +1,54 @@
+//callinng Dom elements
 const main = document.getElementById('main');
 const addUserButton = document.getElementById('add-user');
-const doubleMoneyButton = document.getElementById('double-money');
-const showMillionaireButton = document.getElementById('show-millionaire');
+const doubleMoney = document.getElementById('double-money');
+const showMillionaire = document.getElementById('show-millionaire');
 const sortButton = document.getElementById('sort-richest');
 const totalButton = document.getElementById('calculate-total');
 
-// Initializing Data Array 
 let data = [];
 
+generateRandomUser();
+generateRandomUser();
+generateRandomUser();
 
 //functin to fetch random user
 // used https://randomuser.me/api/
-const generateRandomUser = async () => {
-    const res = await fetch('https://randomuser.me/api/');
+async function generateRandomUser() {
+    const res = await fetch(' https://randomuser.me/api/');
     const data = await res.json();
-    const user = data.results[0];
-    //console.log(user.name);
+     const user = data.results[0];
+     //console.log(user);
     const newUser = {
         name: `${user.name.first} ${user.name.last}`,
         worth: Math.round(Math.random() * 1000000)
-    };
-
-    addData(newUser);
+    }
+    //console.log(newUser);
+     addData(newUser); 
+    
 }
 
-//add newly generated user to data array
-const addData = newUser => {
+//Add newly generated user to data array
+const addData = (newUser) => {
     data.push(newUser);
 
-    updateDom();
+   updateDom();
 }
 
-//console.log(data);
-
 //function to update UI
-const updateDom = (inputdata = data) => {
+const updateDom = (inputData = data) => {
     main.innerHTML = '<h2><strong>Name</strong> NetWorth</h2>';
-    //forEach array method lagarahe hain]
+    //forEach array method lagarhe hain
     //The forEach() method calls a function once for each element in an array, in order.
-    inputdata.forEach((item) => {
+    inputData.forEach( item => {
         const element = document.createElement('div');
         element.classList.add('name');
-        element.innerHTML = `<strong>$(item.name) </strong>`;
+        element.innerHTML = `<strong>${item.name}</strong> ${formatCurrency(item.worth)}`;
+        main.appendChild(element);
     });
 }
 
-
-
-
-
-
-
-
-
-
-
-generateRandomUser();
-generateRandomUser();
-generateRandomUser();
-
-
+//function to format a number as a Currency
+const formatCurrency = num => {
+    return 'PKR ' + (num).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
