3de75b1cabdfc7898fd256b9a8993f4f9f976c95
ebad-ahmed <52370487+ebad-ahmed@users.noreply.github.com>
2021-03-16 15:35:52 +0500

    project 5 completed

--- DOM Array Methods Project 5/script.js
+++ DOM Array Methods Project 5/script.js
@@ -28,6 +28,49 @@ async function generateRandomUser() {
     
 }
 
+
+//Funtion to double the NetWorth of Each User
+const doubleWorth = () => {
+    //The map() method creates a new array with the results of calling a function for every array element.
+    data = data.map((item) => {
+        return { ...item, worth:item.worth * 2 }
+    });
+
+    updateDom();
+}
+
+
+//Funtion to Sort user by the Richest user
+const sortRichest = () => {
+    //The sort order can be either alphabetic or numeric, and either ascending (up) or descending (down).
+    data.sort( (a, b) => b.worth - a.worth);
+
+    updateDom();
+}
+
+//Funtion to show millionaires
+const showMillionaires = () => {
+      //The filter() method creates an array filled with all array elements that pass a test (provided as a function).
+    data = data.filter (item => item.worth > 1000000 );
+
+    updateDom();
+}
+
+
+//Funtion to calculate the total net worth of all users
+const CalculateNetworth = () => {
+    const totalWorth = data.reduce(
+        (acc, item) => (acc += item.worth), 0 
+    );
+    
+    const totalNetworthElement = document.createElement('div');
+    totalNetworthElement.innerHTML = `<h3>Total Net Worth : <strong> ${formatCurrency(totalWorth)} </srong></h3>`
+    main.appendChild(totalNetworthElement);
+}
+
+
+
+
 //Add newly generated user to data array
 const addData = (newUser) => {
     data.push(newUser);
@@ -52,3 +95,15 @@ const updateDom = (inputData = data) => {
 const formatCurrency = num => {
     return 'PKR ' + (num).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
+
+//Event Listners 
+//1.Add User Event Listner
+addUserButton.addEventListener('click', generateRandomUser);
+//2.Add double money event listner
+doubleMoney.addEventListener('click',doubleWorth);
+//3.Add Sort Event Listner
+sortButton.addEventListener('click',sortRichest);
+//4.Add Show Millionaire Event listner
+showMillionaire.addEventListener('click',showMillionaires);
+//5.Add Calculate Total Wealth Event Listner
+totalButton.addEventListener('click',CalculateNetworth);
